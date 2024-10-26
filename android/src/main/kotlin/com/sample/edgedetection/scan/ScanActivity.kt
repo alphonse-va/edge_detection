618aba934e137393c371fda7f2da89ecb7507192
alcapcom <alcapcom@gmail.com>
2024-10-26 14:04:20 +0200

    camera torch permissions seems to be properly handled

--- android/src/main/kotlin/com/sample/edgedetection/scan/ScanActivity.kt
+++ android/src/main/kotlin/com/sample/edgedetection/scan/ScanActivity.kt
@@ -53,8 +53,7 @@ class ScanActivity : BaseActivity(), IScanView.Proxy {
         }
 
         // to hide the flashLight button from  SDK versions which we do not handle the permission for!
-        findViewById<View>(R.id.flash).visibility = if
-                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU && baseContext.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
+        findViewById<View>(R.id.flash).visibility = baseContext.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
             View.VISIBLE else
                 View.GONE
 
